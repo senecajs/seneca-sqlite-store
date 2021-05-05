9cb0ce0d14dbbf89f2ca9238490b86160e4e8ab2
lilsweetcaligula <3748714-lilsweetcaligula@users.noreply.gitlab.com>
2021-05-05 12:20:39 +0300

    Make it easy to run selected tests

--- test/sqlite.test.js
+++ test/sqlite.test.js
@@ -3,26 +3,38 @@
 
 var Seneca = require('seneca')
 var lab = exports.lab = require('@hapi/lab').script()
+var { describe, before } = lab
 var Shared = require('seneca-store-test')
 var SenecaSQLiteStore = require('..')
-var describe = lab.describe
-var si = Seneca(/* {log:'silent'}*/)
-si.use(SenecaSQLiteStore, {database: './test/db/senecatest.db'})
-
 
 describe('sqlite', function () {
-  Shared.basictest({
-    seneca: si,
-    script: lab
+  var si = Seneca(/* {log:'silent'}*/)
+  si.use('entity')
+  si.use(SenecaSQLiteStore, {database: './test/db/senecatest.db'})
+
+  before(() => new Promise((resolve, _reject) => {
+    si.ready(resolve)
+  }))
+
+  describe('basic tests', () => {
+    Shared.basictest({
+      seneca: si,
+      script: lab
+    })
   })
 
-  Shared.sorttest({
-    seneca: si,
-    script: lab
+  describe('sort tests', () => {
+    Shared.sorttest({
+      seneca: si,
+      script: lab
+    })
   })
 
-  Shared.limitstest({
-    seneca: si,
-    script: lab
+  describe('limits tests', () => {
+    Shared.limitstest({
+      seneca: si,
+      script: lab
+    })
   })
 })
+
