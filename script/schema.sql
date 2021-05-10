5a3290247cc9d85b25b974c9ece7d3803adcbcf9
lilsweetcaligula <efim.bogdanovsky@gmail.com>
2021-05-10 20:09:33 +0300

    Update test db schema

--- script/schema.sql
+++ script/schema.sql
@@ -3,7 +3,13 @@
  * sqlite3 -init ./schema.sql ../test/db/senecatest.db
  */
 
-CREATE TABLE foo (id VARCHAR(36), p1 VARCHAR(255), p2 VARCHAR(255), p3 VARCHAR(255), seneca VARCHAR(125));
+CREATE TABLE foo (
+  id VARCHAR(36),
+  p1 VARCHAR(255),
+  p2 VARCHAR(255),
+  p3 VARCHAR(255),
+  seneca VARCHAR(125)
+);
 
 CREATE TABLE moon_bar (
   id VARCHAR(36),
@@ -15,6 +21,21 @@ CREATE TABLE moon_bar (
   `dec` REAL,
   arr TEXT,
   obj TEXT,
-  seneca VARCHAR(125));
+  seneca VARCHAR(125)
+);
 
-CREATE TABLE product (id VARCHAR(36), name VARCHAR(255), price INT);
+CREATE TABLE products (
+  id VARCHAR(36),
+  label VARCHAR(255),
+  price DECIMAL(11, 2)
+);
+
+CREATE TABLE users (
+  id VARCHAR(36),
+  first_name VARCHAR(255),
+  last_name VARCHAR(255),
+  username VARCHAR(255),
+  points INT,
+  skill INT
+);
+
